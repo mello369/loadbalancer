a75477bbab812537189bdb28f9fa2bd588373a03
mello369 <namansingh269@gmail.com>
2022-10-21 15:32:49 +0530

    Added driver function

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mello369/loadbalancer/models"
+	"github.com/mello369/loadbalancer/routes"
+	"github.com/mello369/loadbalancer/utils"
+)
+
+func main() {
+	servers := []models.Server{
+		utils.NewSimpleServer("https://www.facebook.com"),
+		utils.NewSimpleServer("https://www.google.com"),
+		utils.NewSimpleServer("https://www.duckduckgo.com"),
+	}
+	lb := utils.NewLoadBalancer("8000", servers)
+	routes.RegisterRoutes(lb)
+	fmt.Printf("serving requests at port %v", lb.Port)
+	log.Fatal(http.ListenAndServe(":"+lb.Port, nil))
+}
